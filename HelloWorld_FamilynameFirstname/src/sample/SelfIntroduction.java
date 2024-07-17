3f7be076480c86d39d0b9b019642f8bac768e97f
shiraishihayato1031 <tukuyomi1031@outlook.jp>
2024-07-17 22:39:28 +0900

    1-3 新規提出 2024/07/17 白石

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "白石　隼都";
+		// 年齢
+		int age = 23;
+		// 趣味
+		String hobby = "釣り";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}

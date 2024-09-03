939dc6dff7be404149e5bc6e0cf6f5c08f5a71f5
shiraishihayato1031 <tukuyomi1031@outlook.jp>
2024-09-03 23:00:39 +0900

    同じ変数同士の計算がうまくいかない。

---
+++ HelloWorld_FamilynameFirstname/src/JAVA1_8/真偽値.java
@@ -0,0 +1,36 @@
+package JAVA1_8;
+
+public class 真偽値 {
+	public static void main(String[] args) {
+
+byte by=0;
+
+short s = 0;
+
+int i = 0;
+
+long l =0;
+
+float f = 0;
+
+double d = 0;
+
+char c = 0;
+
+String st = "";
+
+boolean bo = false;
+
+System.out.println("byte型は"+by+"です。");
+System.out.println("short型は"+s+"です。");
+System.out.println("int型は"+i+"です。");
+System.out.println("long型は"+l+"です。");
+System.out.println("float型は"+f+"です。");
+System.out.println("double型は"+d+"です。");
+System.out.println("char型は"+c+"です。");
+System.out.println("String型は"+st+"です。");
+System.out.println("boolean型は"+bo+"です。");
+
+}
+	
+};
